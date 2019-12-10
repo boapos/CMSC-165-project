06e064dfcdd62bc68c7fbc4720ba7a4acf7b97eb
boapos <bbapos@up.edu.ph>
2019-12-10 19:15:48 +0800

    done bounding box

--- main_copy.py
+++ main_copy.py
@@ -1,3 +1,4 @@
+#dec 10 - 7:15 progress
 import cv2
 import numpy as np
 
@@ -18,9 +19,13 @@ while(True):
     #cleaning up mask using gaussian filter & dilation
     gaussian = cv2.GaussianBlur(skin,(3,3),0)
     dilated = cv2.dilate(gaussian, None, iterations=11)
+    #extract skin from threshold mask
+    #skin = cv2.bitwise_and(frame, frame, mask=binary_skin)
+    #skin = cv2.cvtColor(skin, cv2.COLOR_HSV2BGR)
 
     #step 3 - differentiate faces - findcontours
     _, contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    #cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
 
     #step 4 - draw bounding box
     for c in contours:
