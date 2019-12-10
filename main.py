67d7ebe9cef0fb5a9b4d9c0b93a494eab5d5872a
boapos <bbapos@up.edu.ph>
2019-12-10 18:50:08 +0800

    done findcontours

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture('https://192.168.254.101:8080/video')
+cap = cv2.VideoCapture('https://192.168.254.106:8080/video')
 
 while(True):
 
     ret, frame = cap.read()
-    #convert frame to hsv
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #step 1 - convert frame to hsv
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    #binarize image
-    #define thresholds
-    lower_threshold = np.array([0,48,80], dtype=np.uint8)
-    upper_threshold = np.array([20,255,255], dtype=np.uint8)
+    #step 2 - binarize image
+    #define thresholds - skin color
+    lower_threshold = np.array([0,58,50])
+    upper_threshold = np.array([30,255,255])
     #single channel mask
-    skinMask = cv2.inRange(frame, lower_threshold, upper_threshold)
-    #cleaning up mask using gaussian filter
-    skinMask = cv2.GaussianBlur(skinMask,(3,3),0)
+    skin = cv2.inRange(hsv, lower_threshold, upper_threshold)
+    #cleaning up mask using gaussian filter & dilation
+    gaussian = cv2.GaussianBlur(skin,(3,3),0)
+    dilated = cv2.dilate(gaussian, None, iterations=9)
     #extract skin from threshold mask
-    #skin = cv2.bitwise_and(frame, frame, mask=skinMask)
+    #skin = cv2.bitwise_and(frame, frame, mask=binary_skin)
     #skin = cv2.cvtColor(skin, cv2.COLOR_HSV2BGR)
 
-    cv2.imshow('stream', skinMask)
+    #step 3 - differentiate faces - findcontours
+    _, contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+
+    cv2.imshow('stream', frame)
 
     #interrupt
     if cv2.waitKey(1) & 0xFF == ord('q'):
